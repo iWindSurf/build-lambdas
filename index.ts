4317e6a72ead3d9ffe4ea1cd095ba3f605215552
Ronald Luitwieler <ronald.luitwieler@aegon.nl>
2020-02-01 08:16:45 +0100

    npm install prod deps only

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
+const archiver = require('archiver')
 import { debug as d } from 'debug'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
+import * as path from 'path'
 import { join } from 'path'
 import * as childProcess from 'child_process'
+import * as glob from 'glob'
+import * as rimraf from 'rimraf'
+import * as crypto from 'crypto'
 import 'array-flat-polyfill'
 
 type JobType = {
@@ -31,15 +36,21 @@ export async function runParallel(jobs: JobType) {
  * ```
  * npm install
  * npm ci
- * npm run build
+ * npm run <script>
  * ```
+ * 
+ * * node_modules folder is nuked before install
+ * * only prod dependencies are installed
  */
 export async function npmRun(npmArgs: string[], cwd: string) {
   d('npmInstall')('')
   const command = 'npm'
   let args = npmArgs.join(' ')
   if ((args === 'install' || args === 'i') && pkgLockExists(cwd)) {
-    args = 'ci'
+    args = 'ci --only=production'
+  } else if (args === 'install' || args === 'i') {
+    rimraf.sync(join(cwd, 'node_modules'))
+    args = 'install --production'
   }
   const options = { cwd: cwd }
   console.log(`Executing ${`${command} ${args}`} in ${cwd}...`)
@@ -90,4 +101,49 @@ async function asyncForEach(array: any, cb: any) {
   for (let index = 0; index < array.length; index++) {
     await cb(array[index], index, array)
   }
+}
+
+export function zipDirectory(directory: string, outputFile: string): Promise<void> {
+  return new Promise(async (ok, fail) => {
+    // The below options are needed to support following symlinks when building zip files:
+    // - nodir: This will prevent symlinks themselves from being copied into the zip.
+    // - follow: This will follow symlinks and copy the files within.
+    const globOptions = {
+      dot: true,
+      nodir: true,
+      follow: true,
+      cwd: directory
+    }
+    const files = glob.sync('**', globOptions) // The output here is already sorted
+
+    const output = fs.createWriteStream(outputFile)
+
+    const archive = archiver('zip')
+    archive.on('warning', fail)
+    archive.on('error', fail)
+    archive.pipe(output)
+
+    // Append files serially to ensure file order
+    for (const file of files) {
+      const fullPath = path.join(directory, file)
+      const [data, stat] = await Promise.all([fs.readFile(fullPath), fs.stat(fullPath)])
+      archive.append(data, {
+        name: file,
+        date: new Date('1980-01-01T00:00:00.000Z'), // reset dates to get the same hash for the same content
+        mode: stat.mode
+      })
+    }
+
+    archive.finalize()
+
+    // archive has been finalized and the output file descriptor has closed, resolve promise
+    output.once('close', () => ok())
+  })
+}
+
+export function contentHash(data: string | Buffer | DataView) {
+  return crypto
+    .createHash('sha256')
+    .update(data)
+    .digest('hex')
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -5,28 +5,26 @@ import * as rimraf from 'rimraf'
 
 test('npm install lambda-no-lock', async () => {
   const lambdaFolder = join(__dirname, 'lambdas', 'lambda-no-lock')
-  rimraf.sync(join(lambdaFolder, 'node_modules'))
   await npmRun(['install'], lambdaFolder)
   expect(fs.existsSync(join(lambdaFolder, 'node_modules', 'debug', 'package.json'))).toBe(true)
+  // devDependencies are not installed.
+  expect(fs.existsSync(join(lambdaFolder, 'node_modules', 'cpu-count', 'package.json'))).toBe(false)
 })
 
 test('npm install lambda-with-lock', async () => {
   const lambdaFolder = join(__dirname, 'lambdas', 'lambda-with-lock')
-  rimraf.sync(join(lambdaFolder, 'node_modules'))
   await npmRun(['install'], lambdaFolder)
   expect(fs.existsSync(join(lambdaFolder, 'node_modules', 'debug', 'package.json'))).toBe(true)
 })
 
 test('npm ci', async () => {
   const lambdaFolder = join(__dirname, 'lambdas', 'lambda-with-lock')
-  rimraf.sync(join(lambdaFolder, 'node_modules'))
   await npmRun(['ci'], lambdaFolder)
   expect(fs.existsSync(join(lambdaFolder, 'node_modules', 'debug', 'package.json'))).toBe(true)
 })
 
 test('npm build', async () => {
   const lambdaFolder = join(__dirname, 'lambdas', 'lambda-no-lock')
-  rimraf.sync(join(lambdaFolder, 'node_modules'))
   const stdout = await npmRun(['run', 'build'], lambdaFolder)
   expect(stdout).toMatch('build succeeded')
 })
@@ -35,26 +33,15 @@ test('run Parallel', async () => {
   const lambdaFolder01 = join('test', 'lambdas', 'lambda-with-lock-01')
   const lambdaFolder02 = join('test', 'lambdas', 'lambda-with-lock-02')
   const lambdaFolder03 = join('test', 'lambdas', 'lambda-with-lock-03')
-  rimraf.sync(join(lambdaFolder01, 'node_modules'))
-  rimraf.sync(join(lambdaFolder02, 'node_modules'))
   rimraf.sync(join(lambdaFolder02, 'build-succeeded.txt'))
-  rimraf.sync(join(lambdaFolder03, 'node_modules'))
 
   await runParallel([
     { args: ['install'], cwd: lambdaFolder01 },
     { args: ['install', ['run', 'build']], cwd: lambdaFolder02 },
     { args: ['install'], cwd: lambdaFolder03 }
   ])
-  expect(fs.existsSync(join(lambdaFolder01, 'node_modules', 'debug', 'package.json'))).toBe(
-    true
-  )
-  expect(fs.existsSync(join(lambdaFolder02, 'node_modules', 'debug', 'package.json'))).toBe(
-    true
-  )
-  expect(fs.existsSync(join(lambdaFolder02, 'build-succeeded.txt'))).toBe(
-    true
-  )
-  expect(fs.existsSync(join(lambdaFolder03, 'node_modules', 'debug', 'package.json'))).toBe(
-    true
-  )
+  expect(fs.existsSync(join(lambdaFolder01, 'node_modules', 'debug', 'package.json'))).toBe(true)
+  expect(fs.existsSync(join(lambdaFolder02, 'node_modules', 'debug', 'package.json'))).toBe(true)
+  expect(fs.existsSync(join(lambdaFolder02, 'build-succeeded.txt'))).toBe(true)
+  expect(fs.existsSync(join(lambdaFolder03, 'node_modules', 'debug', 'package.json'))).toBe(true)
 }, 10000)

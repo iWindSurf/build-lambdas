13fccf7fe3091e545c92464d342a69a9c6a84c0f
Ronald Luitwieler <ronald.luitwieler@aegon.nl>
2020-02-01 15:30:54 +0100

    improving example.js

--- example.js
+++ example.js
@@ -1,21 +1,25 @@
-const join = require('path').join
+const path = require('path')
+const join = path.join
 const buildLambdas = require('.')
 
 const lambdaFolder01 = join('test', 'fixtures', 'lambdas', 'lambda-with-lock-01')
 const lambdaFolder02 = join('test', 'fixtures', 'lambdas', 'lambda-with-lock-02')
 const lambdaFolder03 = join('test', 'fixtures', 'lambdas', 'lambda-with-lock-03')
 
-async function build() {
+async function buildAndZip() {
   await buildLambdas.runParallel([
     { args: ['install'], cwd: lambdaFolder01 },
-    { args: ['install', ['run', 'build']], cwd: lambdaFolder02 },
+    { args: ['install', ['run', 'build'], 'zip'], cwd: lambdaFolder02 },
     { args: ['install'], cwd: lambdaFolder03 }
   ])
 }
 
-async function zip() {
-  await buildLambdas.zipDirectory(lambdaFolder01, 'lambda01.zip')
+async function zip(lambdaFolder) {
+  const zipName = join(process.cwd(), `${path.basename(lambdaFolder)}.zip`)
+  await buildLambdas.zipDirectory(lambdaFolder, zipName)
 }
 
-build()
-zip()
+buildAndZip()
+zip(lambdaFolder01)
+zip(lambdaFolder02)
+zip(lambdaFolder03)

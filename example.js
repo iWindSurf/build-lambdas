d8fe08d20732b5a7897a78d8bfcdd327497e38a5
Ronald Luitwieler <ronald.luitwieler@aegon.nl>
2020-02-01 16:58:17 +0100

    testing

--- example.js
+++ example.js
@@ -8,18 +8,10 @@ const lambdaFolder03 = join('test', 'fixtures', 'lambdas', 'lambda-with-lock-03'
 
 async function buildAndZip() {
   await buildLambdas.runParallel([
-    { args: ['install'], cwd: lambdaFolder01 },
-    { args: ['install', ['run', 'build'], 'zip'], cwd: lambdaFolder02 },
-    { args: ['install'], cwd: lambdaFolder03 }
+    { args: ['install', ['zip']], cwd: lambdaFolder01 },
+    { args: ['install', ['run', 'build'], ['zip']], cwd: lambdaFolder02 },
+    { args: ['install', ['zip']], cwd: lambdaFolder03 }
   ])
 }
 
-async function zip(lambdaFolder) {
-  const zipName = join(process.cwd(), `${path.basename(lambdaFolder)}.zip`)
-  await buildLambdas.zipDirectory(lambdaFolder, zipName)
-}
-
 buildAndZip()
-zip(lambdaFolder01)
-zip(lambdaFolder02)
-zip(lambdaFolder03)

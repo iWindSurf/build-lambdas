6c59e69c9a96a24b47a1f70d68c8ab19f877075a
Ronald Luitwieler <ronald.luitwieler@aegon.nl>
2020-02-07 11:36:26 +0100

    add timeout

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -3,7 +3,7 @@ import * as rimraf from 'rimraf'
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-test('cloud-blocks-cli open-api should show correct help message', async () => {
+test('build-lambdas run', async () => {
   rimraf.sync('test/fixtures/lambdas/lambda-with-lock-cli/test.zip')
 
   await exec(`
@@ -13,4 +13,4 @@ test('cloud-blocks-cli open-api should show correct help message', async () => {
     -o test/fixtures/lambdas/lambda-with-lock-cli/test.zip`)
   expect('1').toMatch('1')
   expect(fs.existsSync('test/fixtures/lambdas/lambda-with-lock-cli/test.zip')).toBe(true)
-})
+}, 10000)
